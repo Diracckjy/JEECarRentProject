bffb5ca681540273953f2f783b06f254a5fb861d
Diracckjy <45199852+Diracckjy@users.noreply.github.com>
2020-09-05 21:30:18 +0800

    为DBLinker增加getConnection方法

--- src/com/carent/dao/DBLinker.java
+++ src/com/carent/dao/DBLinker.java
@@ -5,19 +5,34 @@ package com.carent.dao;
 import com.carent.entity.WebUser;
 
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.sql.Connection;
 import java.sql.SQLException;
 import java.sql.*;
-import java.util.List;
 import java.util.Properties;
 
-public class DBLinker  implements UserDao {
+public class DBLinker {
     // 在登陆时查找给定用户是否存在
-    Connection connection = null;
-    PreparedStatement ps = null;
-    ResultSet resultSet = null;
+
+    private Connection getConnection() throws SQLException {
+        // 数据库用户名和密码以及url
+        String user = "";
+        String url = "";
+        String psw = "";
+        Properties pros = new Properties();
+        try {
+            pros.load(DBLinker.class.getClassLoader().getResourceAsStream("jdbc.propertoes"));
+            url = pros.getProperty("url");
+            user = pros.getProperty("user");
+            psw = pros.getProperty("psw");
+
+            Class.forName(pros.getProperty("driverClass"));
+        } catch (ClassNotFoundException | IOException ex) {
+            ex.printStackTrace();
+        }
+
+        return DriverManager.getConnection(url, user, psw);
+    }
+
     public WebUser findUserInfo(String userName, String password) {
         //声明对象
         WebUser webUser = null;
@@ -25,77 +40,47 @@ public class DBLinker  implements UserDao {
         //声明连接
 
         try {
-            //创建连接
-          //  connection = BjsxtJdbc.getConnection();
-
-
-
-            InputStream is=DBLinker.class.getClassLoader().getResourceAsStream("jdbc.properties");
-            Properties pros=new Properties();
-            pros.load(is);
-            String user = pros.getProperty("user");
-            String password1 = pros.getProperty("password");
-            String url = pros.getProperty("url");
-            String driverClass = pros.getProperty("driverClass");
-            Class.forName(driverClass);
-            connection=DriverManager.getConnection(url,user,password1);
+            Connection conn = getConnection();
 
             String sql = "select * from t_WebUser where userName = ? and password = ? ";
-       //     ps = BjsxtJdbc.getPreparedStatement(connection, sql);
-            ps=connection.prepareStatement(sql);
+            PreparedStatement ps = conn.prepareStatement(sql);
             ps.setString(1, userName);
             ps.setString(2, password);
             //发送sql获取结果集
-            resultSet = ps.executeQuery();
+            ResultSet resultSet = ps.executeQuery();
             //处理结果
             if (resultSet.next()) {
                 int id = resultSet.getInt("id");
                 String uname = resultSet.getString("userName");
                 String upwd = resultSet.getString("password");
-               int rentedCarNum = resultSet.getInt("rentedCarNum");
+                int rentedCarNum = resultSet.getInt("rentedCarNum");
 
                 //创建对象
-                //   WebUser = new WebUser(id, uname, upwd,  clazz);
-                webUser = new WebUser(id ,uname,upwd,rentedCarNum);
+                webUser = new WebUser(id, uname, upwd, rentedCarNum);
 
             }
-
-        } catch (SQLException | IOException | ClassNotFoundException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        } finally {
-//            BjsxtJdbc.closeAll(resultSet, ps, connection);
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
         }
-
 
         return webUser;
     }
 
     // 在注册时存储用户信息
-    public void storeUserInfo(String userName, String password) throws IOException, ClassNotFoundException, SQLException {
-            //sql
+    public void storeUserInfo(String userName, String password) {
 
+        try {
+            Connection connection = getConnection();
+            String sql = "insert into t_WebUser(userName,password,rentedCarNum) values( ?,?,?) ";
+            PreparedStatement ps = connection.prepareStatement(sql);
+            ps.setString(1, userName);
+            ps.setString(2, password);
+            ps.setInt(3, 0);
+            ps.executeUpdate();
 
-        InputStream is=DBLinker.class.getClassLoader().getResourceAsStream("jdbc.properties");
-        Properties pros=new Properties();
-        pros.load(is);
-        String user = pros.getProperty("user");
-        String password1 = pros.getProperty("password");
-        String url = pros.getProperty("url");
-        String driverClass = pros.getProperty("driverClass");
-        Class.forName(driverClass);
-        connection=DriverManager.getConnection(url,user,password1);
-        String sql = "insert into t_WebUser(userName,password,rentedCarNum) values( ?,?,?) ";
-       // String sql = "select * from t_WebUser where userName = ? and password = ? ";
-        //     ps = BjsxtJdbc.getPreparedStatement(connection, sql);
-        ps=connection.prepareStatement(sql);
-        ps.setString(1, userName);
-        ps.setString(2, password);
-        ps.setInt(3, 0);
-
-            //数组
-           // Object[] objs = {w.getId(),w.getUserName(),w.getPassword(),w.getRentedCarNum()};
-            //return BjsxtJdbc.excuteDML(sql, objs);
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
 
     }
 
@@ -133,368 +118,5 @@ public class DBLinker  implements UserDao {
     public void getUserRentedCar() {
 
     }
-
-    @Override
-    public WebUser userloginDao(String name, String pwd) {
-        return null;
-    }
-
-    @Override
-    public int deleteWebUserDao(String WebUserid) {
-        return 0;
-    }
-
-    @Override
-    public int deleteWebUsersDao(String WebUserids) {
-        return 0;
-    }
-
-    @Override
-    public int addWebUserDao(WebUser WebUser) {
-        return 0;
-    }
-
-    @Override
-    public WebUser searchWebUserDao(String WebUserid) {
-        return null;
-    }
-
-    @Override
-    public int updateWebUserDao(WebUser WebUser) {
-        return 0;
-    }
-
-    @Override
-    public List<WebUser> WebUserListDaoLike(String name, String calzz) {
-        return null;
-    }
-
-    @Override
-    public int selectCountDao() {
-        return 0;
-    }
-//
-//    public class WebUserDaoImpl implements UserDao {
-//
-//        //登陆
-//        @Override
-//        public WebUser WebUserloginDao(String name, String pwd) {
-//            //声明对象
-//            WebUser WebUser = null;
-//            //jdbc
-//            //声明连接
-//            Connection connection = null;
-//            PreparedStatement ps = null;
-//            ResultSet resultSet = null;
-//            try {
-//                //创建连接
-//                connection = BjsxtJdbc.getConnection();
-//                String sql = "select * from t_WebUser where name = ? and pwd = ? ";
-//                ps = BjsxtJdbc.getPreparedStatement(connection, sql);
-//                ps.setString(1, name);
-//                ps.setString(2, pwd);
-//                //发送sql获取结果集
-//                resultSet = ps.executeQuery();
-//                //处理结果
-//                if (resultSet.next()) {
-//                    String id = resultSet.getString("id");
-//                    String uname = resultSet.getString("name");
-//                    String upwd = resultSet.getString("pwd");
-//                    int age = resultSet.getInt("age");
-//                    int sex = resultSet.getInt("sex");
-//                    Date enterdate = resultSet.getDate("enterdate");
-//                    String clazz = resultSet.getString("clazz");
-//                    //创建对象
-//                    // WebUser = new WebUser(id, uname, upwd, age, sex, enterdate, clazz);
-//                }
-//
-//            } catch (SQLException e) {
-//                // TODO Auto-generated catch block
-//                e.printStackTrace();
-//            } finally {
-//                BjsxtJdbc.closeAll(resultSet, ps, connection);
-//            }
-//
-//
-//            return WebUser;
-//        }
-
-//        //查询所有学生信息
-//        @Override
-//        public void WebUserListDao(SxtPageUtil<WebUser> sxtPageUtil) {
-//            //声明对象
-//            List<WebUser> WebUserList = new ArrayList<>();
-//            //jdbc
-//            //声明连接
-//            Connection connection = null;
-//            PreparedStatement ps = null;
-//            ResultSet resultSet = null;
-//            try {
-//                //创建连接
-//                connection = BjsxtJdbc.getConnection();
-//                StringBuilder sql = new StringBuilder("select * from t_WebUser where 1=1 ");
-//                sql.append(" limit "+ sxtPageUtil.getStart() +","+ sxtPageUtil.getSize());
-//                ps = BjsxtJdbc.getPreparedStatement(connection, sql);
-//                //发送sql获取结果集
-//                resultSet = ps.executeQuery();
-//                //处理结果
-//                while(resultSet.next()){
-//                    String id = resultSet.getString("id");
-//                    String uname = resultSet.getString("name");
-//                    String upwd = resultSet.getString("pwd");
-//                    int age = resultSet.getInt("age");
-//                    int sex = resultSet.getInt("sex");
-//                    Date enterdate = resultSet.getDate("enterdate");
-//                    String clazz = resultSet.getString("clazz");
-//                    //创建对象
-//                    WebUser WebUser = new WebUser(id, uname, upwd, age, sex, enterdate, clazz);
-//                    //把对象存放到集合中
-//                    WebUserList.add(WebUser);
-//                }
-//
-//            } catch (SQLException e) {
-//                // TODO Auto-generated catch block
-//                e.printStackTrace();
-//            }finally{
-//                BjsxtJdbc.closeAll(resultSet, ps, connection);
-//            }
-//
-//            sxtPageUtil.setList(WebUserList);
-//            //return WebUserList;
-//        }
-//
-//        //查询所有班级信息
-//        @Override
-//        public List<Clazz> clazzListDao() {
-//            //声明对象
-//            List<Clazz> clazzList = new ArrayList<>();
-//            //jdbc
-//            //声明连接
-//            Connection connection = null;
-//            PreparedStatement ps = null;
-//            ResultSet resultSet = null;
-//            try {
-//                //创建连接
-//                connection = BjsxtJdbc.getConnection();
-//                String sql = "select * from t_class  ";
-//                ps = BjsxtJdbc.getPreparedStatement(connection, sql);
-//                //发送sql获取结果集
-//                resultSet = ps.executeQuery();
-//                //处理结果
-//                while(resultSet.next()){
-//                    String id = resultSet.getString("id");
-//                    String uname = resultSet.getString("name");
-//                    //创建对象
-//                    Clazz clazz = new Clazz(id, uname);
-//                    //把对象存放到集合中
-//                    clazzList.add(clazz);
-//                }
-//
-//            } catch (SQLException e) {
-//                // TODO Auto-generated catch block
-//                e.printStackTrace();
-//            }finally{
-//                BjsxtJdbc.closeAll(resultSet, ps, connection);
-//            }
-//            return clazzList;
-//        }
-//
-//        @Override
-//        public WebUser userloginDao(String name, String pwd) {
-//            return null;
-//        }
-//
-//        //删除学生信息
-//        @Override
-//        public int deleteWebUserDao(String WebUserid) {
-//            //sql
-//            String sql = "delete from t_WebUser where id = ? ";
-//            //数组
-//            Object[] objs = {WebUserid};
-//            return BjsxtJdbc.excuteDML(sql, objs);
-//        }
-//
-//        //批量删除学生信息
-//        @Override
-//        public int deleteWebUsersDao(String WebUserids) {
-//
-//            //数组
-//            String newWebUserids = WebUserids.substring(0, WebUserids.length()-1);
-//            Object[] objs = newWebUserids.split(",");
-//
-//            //sql
-//            StringBuilder sql = new StringBuilder("delete from t_WebUser where id in (  " ) ;
-//            //拼接数组
-//            for (int i = 0; i < objs.length; i++) {
-//                if(i==objs.length-1){
-//                    sql.append(" ? )" );
-//                }else{
-//                    sql.append(" ? , " );
-//                }
-//            }
-//
-//            System.out.println("WebUserDaoImpl.deleteWebUsersDao(sql):"+sql);
-//
-//            return BjsxtJdbc.excuteDML(sql, objs);
-//        }
-//
-//        //添加学生信息
-//        @Override
-//        public int addWebUserDao(WebUser s) {
-//            //sql
-//            String sql = "insert into t_WebUser(id,name,pwd,age,sex,enterdate,clazz) values( ?,?,?,?,?,?,? ) ";
-//
-//            //数组
-//            Object[] objs = {s.getId(),s.getName(),s.getPwd(),s.getAge(),s.getSex(),s.getEnterdate(),s.getClazz()};
-//            return BjsxtJdbc.excuteDML(sql, objs);
-//        }
-//
-//        //查询单个学生的信息--根据id查询
-//        @Override
-//        public WebUser searchWebUserDao(String WebUserid) {
-//            //声明对象
-//            WebUser WebUser = null;
-//            //jdbc
-//            //声明连接
-//            Connection connection = null;
-//            PreparedStatement ps = null;
-//            ResultSet resultSet = null;
-//            try {
-//                //创建连接
-//                connection = BjsxtJdbc.getConnection();
-//                String sql = "select * from t_WebUser where id = ? ";
-//                ps = BjsxtJdbc.getPreparedStatement(connection, sql);
-//                ps.setString(1, WebUserid);
-//
-//                //发送sql获取结果集
-//                resultSet = ps.executeQuery();
-//                //处理结果
-//                if(resultSet.next()){
-//                    String id = resultSet.getString("id");
-//                    String uname = resultSet.getString("name");
-//                    String upwd = resultSet.getString("pwd");
-//                    int age = resultSet.getInt("age");
-//                    int sex = resultSet.getInt("sex");
-//                    Date enterdate = resultSet.getDate("enterdate");
-//                    String clazz = resultSet.getString("clazz");
-//                    //创建对象
-//                    WebUser = new WebUser(id, uname, upwd, age, sex, enterdate, clazz);
-//                }
-//
-//            } catch (SQLException e) {
-//                // TODO Auto-generated catch block
-//                e.printStackTrace();
-//            }finally{
-//                BjsxtJdbc.closeAll(resultSet, ps, connection);
-//            }
-//
-//            return WebUser;
-//
-//        }
-//
-//        //修改学生信息
-//        @Override
-//        public int updateWebUserDao(WebUser s) {
-//            //sql
-//            String sql = "update t_WebUser set name = ? ,pwd = ? ,age = ?,sex = ?,enterdate = ?,clazz = ? where id = ? ";
-//            //数组
-//            Object[] objs = {s.getName(),s.getPwd(),s.getAge(),s.getSex(),s.getEnterdate(),s.getClazz(),s.getId()};
-//
-//            return BjsxtJdbc.excuteDML(sql, objs);
-//        }
-//
-//        //模糊查询
-//        @Override
-//        public List<WebUser> WebUserListDaoLike(String name, String clazz) {
-//            //声明对象
-//            List<WebUser> WebUserList = new ArrayList<>();
-//            //jdbc
-//            //声明连接
-//            Connection connection = null;
-//            PreparedStatement ps = null;
-//            ResultSet resultSet = null;
-//            try {
-//                //创建连接
-//                connection = BjsxtJdbc.getConnection();
-//                //sql
-//                StringBuilder sql =  new StringBuilder("select * from t_WebUser where 1 = 1 ") ;
-//                //判断传入的参数是否为空
-//                if((name!=null&&!"".equals(name))&&(clazz!=null&&!"".equals(clazz))){
-//                    sql.append(" and name like ? and clazz like ? ");
-//                    ps = BjsxtJdbc.getPreparedStatement(connection, sql);
-//                    ps.setString(1, "%"+name+"%");
-//                    ps.setString(2, "%"+clazz+"%");
-//
-//                }else if((name!=null&&!"".equals(name))&&(clazz==null||"".equals(clazz))){
-//                    sql.append(" and name like ?  ");
-//                    ps = BjsxtJdbc.getPreparedStatement(connection, sql);
-//                    ps.setString(1, "%"+name+"%");
-//                }else if((name==null||"".equals(name))&&(clazz!=null&&!"".equals(clazz))){
-//                    sql.append(" and clazz like ?  ");
-//                    ps = BjsxtJdbc.getPreparedStatement(connection, sql);
-//                    ps.setString(1, "%"+clazz+"%");
-//                }else{
-//                    ps = BjsxtJdbc.getPreparedStatement(connection, sql);
-//                }
-//                //发送sql获取结果集
-//                resultSet = ps.executeQuery();
-//                //处理结果
-//                while(resultSet.next()){
-//                    String id = resultSet.getString("id");
-//                    String uname = resultSet.getString("name");
-//                    String upwd = resultSet.getString("pwd");
-//                    int age = resultSet.getInt("age");
-//                    int sex = resultSet.getInt("sex");
-//                    Date enterdate = resultSet.getDate("enterdate");
-//                    String clazz2 = resultSet.getString("clazz");
-//                    //创建对象
-//                    WebUser WebUser = new WebUser(id, uname, upwd, age, sex, enterdate, clazz2);
-//                    //把对象存放到集合中
-//                    WebUserList.add(WebUser);
-//                }
-//
-//            } catch (SQLException e) {
-//                // TODO Auto-generated catch block
-//                e.printStackTrace();
-//            }finally{
-//                BjsxtJdbc.closeAll(resultSet, ps, connection);
-//            }
-//            return WebUserList;
-//
-//        }
-//
-//        //查询总记录数
-//        @Override
-//        public int selectCountDao() {
-//            //声明一个变量
-//            int total = 0;
-//            //jdbc
-//            //声明连接
-//            Connection connection = null;
-//            PreparedStatement ps = null;
-//            ResultSet resultSet = null;
-//            try {
-//                //创建连接
-//                connection = BjsxtJdbc.getConnection();
-//                String sql = "select count(*) from t_WebUser ";
-//                ps = BjsxtJdbc.getPreparedStatement(connection, sql);
-//                //发送sql获取结果集
-//                resultSet = ps.executeQuery();
-//                //处理结果
-//                if(resultSet.next()){
-//                    total = resultSet.getInt(1);
-//                }
-//
-//            } catch (SQLException e) {
-//                // TODO Auto-generated catch block
-//                e.printStackTrace();
-//            }finally{
-//                BjsxtJdbc.closeAll(resultSet, ps, connection);
-//            }
-//            return total;
-//        }
-//
-//    }
-
 
 }
