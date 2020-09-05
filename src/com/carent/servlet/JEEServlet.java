1161995e1130a16dae97a93a56ba2d74eede8a41
Sun-Sir <1228957747@qq.com>
2020-09-05 14:55:00 +0800

    servlet ok

--- src/com/carent/servlet/JEEServlet.java
+++ src/com/carent/servlet/JEEServlet.java
@@ -1,13 +1,14 @@
 package com.carent.servlet;
 
 import com.carent.service.JEEService;
-
+import com.carent.entity.WebUser;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
+import com.carent.service.JEEService;
 
 public class JEEServlet extends HttpServlet {
     @Override
@@ -23,32 +24,28 @@ public class JEEServlet extends HttpServlet {
 
         }
         else if("register".equals(opa)){
-            int a=userRegister(req,resp,name,psw);
-            if (a==1){
-                req.getRequestDispatcher("/login.jsp").forward(req,resp);
+            userRegister(req,resp,name,psw);
             }
-            else{
-                pw.print("注册失败");
-            }
-        }
+
         else if ("rentCar".equals(opa)){
 
         }
     }
 
     // 登录成功后重定向页面到主页
-    public void userLogin(HttpServletRequest req, HttpServletResponse resp,
-                         String userName, String password)throws IOException,ServletException
+    public void userLogin(HttpServletRequest req, HttpServletResponse resp, String userName, String password)
+            throws IOException,ServletException
     {
-        int userId = -1;
+
         PrintWriter pw=resp.getWriter();
         JEEService jes = new JEEService();
-
-        userId = jes.LoginService(userName, password);
-        if (userId==1){
-            req.getRequestDispatcher("/rentCar.jsp").forward(req,resp);
+        WebUser userId= jes.loginService(userName, password);
+        if (userId==null){
+            req.getRequestDispatcher("/login.jsp").forward(req,resp);
         }
-        else:
+        else{
+            req.getRequestDispatcher("/returnCar.jsp").forward(req,resp);
+        }
 
     }
 
